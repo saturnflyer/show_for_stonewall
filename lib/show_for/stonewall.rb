7c7c366acdf51c7e6abf7ed6d009c63fd886be59
Jim Gay <jim@saturnflyer.com>
2010-06-21 17:38:54 -0400

    oops

--- lib/show_for/stonewall.rb
+++ lib/show_for/stonewall.rb
@@ -4,8 +4,6 @@ module ShowFor
       apply_default_options!(attribute_name, options)
       collection_block, block = block, nil if collection_block?(block)
       
-      return wrap_label_and_content(attribute_name, (I18n.t :"show_for.redacted", :default => 'Redacted'), options, &collection_block)
-      
       if @object.respond_to?(:send?) && !allowed?(@object, current_user, attribute_name)
         wrap_label_and_content(attribute_name, (I18n.t :"show_for.redacted", :default => "Redacted"), options, &collection_block)
       else
